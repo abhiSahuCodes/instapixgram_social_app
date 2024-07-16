c5d4b908b9bdf089275fd225b355915988958f27
Abhishek Sahu <abhishek963sahu@gmail.com>
2024-07-16 23:05:49 +0530

    Add sign up and saving new user data to database

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,7 +1,8 @@
 import { INewUser } from "@/types";
 import { ID } from "appwrite";
-import { account } from "./config";
+import { account, appwriteConfig, avatars, databases } from "./config";
 
+// Sign up
 export async function createUserAccount(user: INewUser) {
   try {
     const newAccount = await account.create(
@@ -11,9 +12,43 @@ export async function createUserAccount(user: INewUser) {
       user.name
     );
 
-    return newAccount;
+    if (!newAccount) throw Error;
+
+    const avatarUrl = avatars.getInitials(user.name);
+
+    const newUser = await saveUserToDB({
+      accountId: newAccount.$id,
+      name: newAccount.name,
+      email: newAccount.email,
+      username: user.username,
+      imageUrl: avatarUrl,
+    });
+
+    return newUser;
   } catch (error) {
     console.log(error);
     return error;
+  }
+}
+
+// Saving user to database
+export async function saveUserToDB(user: {
+  accountId: string;
+  email: string;
+  name: string;
+  imageUrl: URL;
+  username?: string;
+}) {
+  try {
+    const newUser = await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.usersCollectionId,
+      ID.unique(),
+      user
+    );
+
+    return newUser;
+  } catch (error) {
+    console.log(error);
   }
 }
