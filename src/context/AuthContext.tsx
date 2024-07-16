d6ecd9fdcf12637bf9f6a5affe85dd8a5d465cde
Abhishek Sahu <abhishek963sahu@gmail.com>
2024-07-16 23:59:37 +0530

    Add initial auth context

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,39 @@
+import { createContext, useState } from "react";
+
+export const INITIAL_USER = {
+  id: "",
+  name: "",
+  username: "",
+  email: "",
+  imageUrl: "",
+  bio: "",
+};
+
+const INITIAL_STATE = {
+  user: INITIAL_USER,
+  isLoading: false,
+  isAuthenticated: false,
+  setUser: () => {},
+  setIsAuthenticated: () => {},
+  checkAuthUser: async () => false as boolean,
+};
+
+const AuthContext = createContext<IContextType>(INITIAL_STATE);
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+    const [user, setUser] = useState<IUser>(INITIAL_USER);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+
+    
+
+    const value = {
+        user,
+        setUser,
+        isLoading,
+        isAuthenticated,
+        setIsAuthenticated,
+      };
+
+      return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
