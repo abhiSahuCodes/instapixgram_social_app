e6023a89e6e7ec0bcf8b7fd03fabd8c30e458093
Abhishek Sahu <abhishek963sahu@gmail.com>
2024-07-19 20:20:49 +0530

    Fix pages

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -1,5 +1,6 @@
 import * as z from "zod";
 
+// USER VALIDATION
 export const SignupValidation = z.object({
   name: z.string().min(2, { message: "Name must be at least 2 characters." }),
   username: z
@@ -18,9 +19,26 @@ export const SigninValidation = z.object({
     .min(8, { message: "Password must be at least 8 characters." }),
 });
 
-export const PostValidation = z.object({
-  caption: z.string().min(5, { message: "Minimum 5 characters." }).max(2200, { message: "Maximum 2,200 caracters" }),
+export const ProfileValidation = z.object({
   file: z.custom<File[]>(),
-  location: z.string().min(1, { message: "This field is required" }).max(1000, { message: "Maximum 1000 characters." }),
+  name: z.string().min(2, { message: "Name must be at least 2 characters." }),
+  username: z
+    .string()
+    .min(2, { message: "Name must be at least 2 characters." }),
+  email: z.string().email(),
+  bio: z.string(),
+});
+
+// POST VALIDATION
+export const PostValidation = z.object({
+  caption: z
+    .string()
+    .min(5, { message: "Minimum 5 characters." })
+    .max(2200, { message: "Maximum 2,200 caracters" }),
+  file: z.custom<File[]>(),
+  location: z
+    .string()
+    .min(1, { message: "This field is required" })
+    .max(1000, { message: "Maximum 1000 characters." }),
   tags: z.string(),
 });
